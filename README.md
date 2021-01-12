059e9ea212ddb86bd633a63bf78974fbd6830dd4
Dat Pham <datmpham96@gmail.com>
2021-01-11 20:01:42 -0600

    track functionality

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# music-search-api-capstone
+# Music Search Api Capstone (to do now)
+This capstone project will be designed to help a user search for event venues based on location and event location types while displaying 
+useful details about the venue. 
+
+## 1. Working Prototype (to do now)
+You can access a working prototype here: https://nikosamofa.github.io/venue-search-api-capstone
+
+
+## 2. User Stories 
+This app is for a single type of user: a visitor
+
+#### Landing Page (Importance - High)(est:2h)
+* as a visitor
+* I want to understand what I can do with this app
+* so I can decide if I want to use it
+
+#### Main Page (Importance - High)(est:4h)
+* as a visitor
+* I want to search for track,artist, and album
+* so I can decide what I want to listen
+
+#### Result Section (Importance - High)(est:1h)
+* as a visitor
+* I want to preview a list search result(tracks,artist album cover)
+* so I can make a decision based on the information returned
+
+## 3. Functionality 
+The app's functionality includes:
+* each user should land on the homepage of the application to understand what is the application is about
+* each user should be able to search for track, artist and album
+* each user should be able to click on search result to see whats in there
+
+## 4. Technology 
+* Front-End: HTML5, CSS3, JavaScript, jQuery, Api
+* Development Environment: Github
+
+### 5. Wireframes (to do now)
+(Example) Landing Page
+:-------------------------:
+![Landing Page](/github-images/wireframes/landing-page-wireframe.jpg)
+Main Page
+![Register Page](/github-images/wireframes/main-page-wireframe.jpg)
+
+## 6. API Documentation 
+API Documentation details:
+* Venue Search (Returns a list of venues near the current location, optionally matching a search term.) https://developer.foursquare.com/docs/api-reference/venues/search/
+
+
+
+## 7. Screenshots (to do later)
+(Example) Landing Page
+:-------------------------:
+![Landing Page](/github-images/screenshots/landing-page-screenshot.png)
+Register Page
+![Search Page](/github-images/screenshots/search-page-screenshot.png)
+
+## 8. Development Roadmap (to do later)
+This is v1.0 of the app, but future enhancements are expected to include:
+* (Example) add more functionality
---
+++ app.js
@@ -0,0 +1,179 @@
+'use strict';
+
+
+const searchArtistUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=5f9a6038d66d4bba8b7e4a2a867a1226&format=json`
+const searchAlbumUrl = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=5f9a6038d66d4bba8b7e4a2a867a1226&format=json`
+
+
+// start track functionality
+function displayTrackResults(responseJson) {
+    console.log(responseJson);
+    $('#results-track-list').empty();
+    let html = '';
+    for (let i = 0; i < responseJson.results.trackmatches.track.length; i++) {
+        console.log(responseJson.results.trackmatches.track[i]);
+
+        html += `
+        <li><h3>${responseJson.results.trackmatches.track[i].name}</h3>
+          <p>Description: ${responseJson.results.trackmatches.track[i].artist}</p>
+          <p>URL : <a href="${responseJson.results.trackmatches.track[i].url}">${responseJson.results.trackmatches.track[i].url}</a></p>
+          <img src="${responseJson.results.trackmatches.track[i].image[3]['#text']}" class="results-image" alt="${responseJson.results.trackmatches.track[i].name}">
+        </li>`;
+    }
+
+    $('#results-track-list').html(html);
+    $('#results').removeClass('hidden');
+}
+
+function getTrackResults(query) {
+    const searchTrackUrl = `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${query}&api_key=5f9a6038d66d4bba8b7e4a2a867a1226&format=json`
+    console.log(searchTrackUrl);
+
+
+    fetch(searchTrackUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayTrackResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+function watchTrackForm() {
+    $('.track-form').submit(function (event) {
+        event.preventDefault();
+        const track = $(this)
+            .find('#track')
+            .val();
+
+        console.log(track)
+        getTrackResults(track);
+    });
+}
+// stop track functionality
+
+
+
+
+
+
+
+
+
+
+// start artist functionality
+function displayArtistResults(responseJson) {
+    console.log(responseJson);
+    $('#results-artist-list').empty();
+    let html = '';
+    for (let i = 0; i < responseJson.results.artistmatches.artist.length; i++) {
+        console.log(responseJson.results.artistmatches.artist[i]);
+
+        html += `
+        <li><h3>${responseJson.results.artistmatches.artist[i].name}</h3>
+          <p>Description: ${responseJson.results.artistmatches.artist[i].artist}</p>
+          <p>URL : <a href="${responseJson.results.artistmatches.artist[i].url}">${responseJson.results.artistmatches.artist[i].url}</a></p>
+          <img src="${responseJson.results.artistmatches.artist[i].image[3]['#text']}" class="results-image" alt="${responseJson.results.artistmatches.artist[i].name}">
+        </li>`;
+    }
+
+    $('#results-artist-list').html(html);
+    $('#results').removeClass('hidden');
+}
+
+function getArtistResults(query) {
+    const searchArtistUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${query}&api_key=5f9a6038d66d4bba8b7e4a2a867a1226&format=json`
+    console.log(searchArtistUrl);
+
+
+    fetch(searchArtistUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayArtistResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+function watchArtistForm() {
+    $('.artist-form').submit(function (event) {
+        event.preventDefault();
+        const artist = $(this)
+            .find('#artist')
+            .val();
+
+        console.log(artist)
+        getArtistResults(artist);
+    });
+}
+// stop artist functionality
+
+
+
+
+
+
+
+
+
+// start album functionality
+function displayAlbumResults(responseJson) {
+    console.log(responseJson);
+    $('#results-album-list').empty();
+    let html = '';
+    for (let i = 0; i < responseJson.results.albummatches.album.length; i++) {
+        console.log(responseJson.results.albummatches.album[i]);
+
+        html += `
+        <li><h3>${responseJson.results.albummatches.album[i].name}</h3>
+          <p>Description: ${responseJson.results.albummatches.album[i].album}</p>
+          <p>URL : <a href="${responseJson.results.albummatches.album[i].url}">${responseJson.results.albummatches.album[i].url}</a></p>
+          <img src="${responseJson.results.albummatches.album[i].image[3]['#text']}" class="results-image" alt="${responseJson.results.albummatches.album[i].name}">
+        </li>`;
+    }
+
+    $('#results-album-list').html(html);
+    $('#results').removeClass('hidden');
+}
+
+function getAlbumResults(query) {
+    const searchAlbumUrl = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=5f9a6038d66d4bba8b7e4a2a867a1226&format=json`
+    console.log(searchAlbumUrl);
+
+
+    fetch(searchAlbumUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayAlbumResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+function watchAlbumForm() {
+    $('.album-form').submit(function (event) {
+        event.preventDefault();
+        const album = $(this)
+            .find('#album')
+            .val();
+
+        console.log(album)
+        getAlbumResults(album);
+    });
+}
+// stop album functionality
+
+$(watchTrackForm);
+
